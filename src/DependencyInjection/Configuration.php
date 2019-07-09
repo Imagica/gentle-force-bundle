e2db7ee983a00c9ff9127ec46e23aa0a425a46cc
Imagica <david@daviddale.net>
2019-07-09 10:04:46 +0100

    Fix for Deprecation notice with Symfony 4.2
    https://github.com/mariusbalcytis/gentle-force-bundle/issues/15

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -12,8 +12,8 @@ class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('maba_gentle_force');
+        $treeBuilder = new TreeBuilder('maba_gentle_force');
+        $rootNode = method_exists(TreeBuilder::class, 'getRootNode') ? $treeBuilder->getRootNode() : $treeBuilder->root('maba_gentle_force');        
 
         $children = $rootNode->children();
 
